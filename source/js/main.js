17206ff94ec0e2aece96e203bd8510e8ae2f5f18
Frank Wang <frank.wangtao@gmail.com>
2014-09-25 12:12:57 -0400

    Animate icons

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,42 @@
+(function() {
+    $(document).ready(function() {
+	// Declare and initialize buttons
+	var $intro = $("div.intro");
+	var $morning = $("div.morning");
+	var $school = $("div.school");
+	var $evening = $("div.evening");
 
+	// Declare and initialize icons
+	var $bike = $("svg.bike");
+	var $train = $("svg.train");
+	var $walk = $("div.walk");
+
+	$intro.on("click", "svg.arrow", function() {
+	    $bike.attr("class", "bike commute downhill-left");
+	    setTimeout(function() {
+		smoothScroll.animateScroll(null, "#morning");
+	    }, 2000);
+	});
+	
+	$morning.on("click", "svg.arrow", function() {
+	    $train.attr("class", "train commute downhill-right");
+	    setTimeout(function() {
+		smoothScroll.animateScroll(null, "#school");
+	    }, 2000);
+	});
+
+	$school.on("click", "svg.arrow", function() {
+	    $walk.addClass("walking");
+	    setTimeout(function() {
+		smoothScroll.animateScroll(null, "#evening");
+	    }, 3000);
+	});
+
+	$evening.on("click", "svg", function() {
+	    $walk.removeClass("walking");
+	    $train.attr("class", "train commute");
+	    $bike.attr("class", "bike commute");
+	    smoothScroll.animateScroll(null, "#top");
+	});
+    });
+})();

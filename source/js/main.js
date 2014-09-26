21bba9656ce95087f1eee2ccd833d19271fffabf
Frank Wang <frank.wangtao@gmail.com>
2014-09-26 12:09:18 -0400

    Activate animation when following section visible

--- source/js/main.js
+++ source/js/main.js
@@ -37,6 +37,41 @@
 	    $train.attr("class", "train commute");
 	    $bike.attr("class", "bike commute");
 	    smoothScroll.animateScroll(null, "#top");
+
+	// Returns true if element is visible in viewport
+	function isScrolledIntoView(elem) {
+	    var docViewTop = $(window).scrollTop();
+	    var docViewBottom = docViewTop + $(window).height();
+
+	    var elemTop = $(elem).offset().top;
+	    var elemBottom = elemTop + $(elem).height();
+	    return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+	}
+
+	$(window).on("scroll", function () {	    
+	    // Activate animation when next section scrolls into view
+	    if (isScrolledIntoView("#morning")) {
+		$bike.attr("class", "bike commute downhill-left");
+	    }
+	    if (isScrolledIntoView("#school")) {
+		$train.attr("class", "train commute downhill-right");
+	    }
+	    if (isScrolledIntoView("#evening")) {
+		$walk.removeClass("no-transition");
+		$walk.addClass("walking");
+	    }
+	    
+	    // Reset animation when icon scrolls out of view
+	    if (!isScrolledIntoView($bike)) {
+		$bike.attr("class", "bike commute no-transition");
+	    }
+	    if (!isScrolledIntoView($train)) {
+		$train.attr("class", "train commute no-transition");
+	    }
+	    if (!isScrolledIntoView($walk)) {
+		$walk.addClass("no-transition");
+		$walk.removeClass("walking");
+	    }
 	});
     });
 })();
